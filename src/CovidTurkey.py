beb0893a9afca64fb50a615a13a828889b9cf6b5
egeakman <egeakmanegeakman@hotmail.com>
2021-12-11 14:47:26 +0300

    oops

--- src/CovidTurkey.py
+++ src/CovidTurkey.py
@@ -61,34 +61,34 @@ class covid_turkey:
         return data[0].text
 
     def get_first_dose_count(self):
-        return self.update_vaccination_data(data_raw="doz1asisayisi")
+        return self.update_vaccination_data(data_arg="doz1asisayisi")
 
     def get_first_dose_percent(self):
-        return self.update_vaccination_data(data_raw="dozturkiyeortalamasi")
+        return self.update_vaccination_data(data_arg="dozturkiyeortalamasi")
 
     def get_second_dose_count(self):
-        return self.update_vaccination_data(data_raw="doz2asisayisi")
+        return self.update_vaccination_data(data_arg="doz2asisayisi")
 
     def get_second_dose_percent(self):
-        return self.update_vaccination_data(data_raw="doz2turkiyeortalamasi")
+        return self.update_vaccination_data(data_arg="doz2turkiyeortalamasi")
 
     def get_third_dose_count(self):
-        return self.update_vaccination_data(data_raw="doz3asisayisi")
+        return self.update_vaccination_data(data_arg="doz3asisayisi")
 
     def get_third_dose_percent(self):
-        return self.update_vaccination_data(data_raw="doz3turkiyeortalamasi")
+        return self.update_vaccination_data(data_arg="doz3turkiyeortalamasi")
 
     def get_fourth_dose_count(self):
-        return self.update_vaccination_data(data_raw="doz4asisayisi")
+        return self.update_vaccination_data(data_arg="doz4asisayisi")
 
     def get_fourth_dose_percent(self):
-        return self.update_vaccination_data(data_raw="doz4turkiyeortalamasi")
+        return self.update_vaccination_data(data_arg="doz4turkiyeortalamasi")
 
     def get_total_dose_count(self):
-        return self.update_vaccination_data(data_raw="toplamasidozusayisi")
+        return self.update_vaccination_data(data_arg="toplamasidozusayisi")
 
     def get_daily_dose_count(self):
-        return self.update_vaccination_data(data_raw="gunluksidozusayisi")
+        return self.update_vaccination_data(data_arg="gunluksidozusayisi")
 
     def get_last_update(self):
-        return self.update_vaccination_data(data_raw="asidozuguncellemesaati")
+        return self.update_vaccination_data(data_arg="asidozuguncellemesaati")
