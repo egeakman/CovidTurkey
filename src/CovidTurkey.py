8563a1e543b1c5a80a8a26850996be5b7a3a0af2
Arda Sak <ardasak434@gmail.com>
2021-12-21 17:42:14 +0300

    Bettered ig?

--- src/CovidTurkey.py
+++ src/CovidTurkey.py
@@ -5,62 +5,65 @@ from src import functions
 class covid_turkey:
     class vaccination:
         def get_first_dose_count(self, formatted = False):
-            return functions.remove_dots_and_commas(functions.update_vaccination_data(data_arg="doz1asisayisi"), formatted)
+            return functions.update_vaccination_data(data_arg="doz1asisayisi", formatted=formatted)
 
         def get_first_dose_percent(self):
             return functions.update_vaccination_data(data_arg="dozturkiyeortalamasi")
 
         def get_second_dose_count(self, formatted = False):
-            return functions.remove_dots_and_commas(functions.update_vaccination_data(data_arg="doz2asisayisi"), formatted)
+            return functions.update_vaccination_data(data_arg="doz2asisayisi", formatted=formatted)
 
         def get_second_dose_percent(self):
             return functions.update_vaccination_data(data_arg="doz2turkiyeortalamasi")
 
         def get_third_dose_count(self, formatted = False):
-            return functions.remove_dots_and_commas(functions.update_vaccination_data(data_arg="doz3asisayisi"), formatted)
+            return functions.update_vaccination_data(data_arg="doz3asisayisi", formatted=formatted)
 
         def get_third_dose_percent(self):
             return functions.update_vaccination_data(data_arg="doz3turkiyeortalamasi")
 
         def get_fourth_dose_count(self, formatted = False):
-            return functions.remove_dots_and_commas(functions.update_vaccination_data(data_arg="doz4asisayisi"), formatted)
+            return functions.update_vaccination_data(data_arg="doz4asisayisi", formatted=formatted)
 
         def get_fourth_dose_percent(self):
             return functions.update_vaccination_data(data_arg="doz4turkiyeortalamasi")
 
         def get_total_dose_count(self, formatted = False):
-            return functions.remove_dots_and_commas(functions.update_vaccination_data(data_arg="toplamasidozusayisi"), formatted)
+            return functions.update_vaccination_data(data_arg="toplamasidozusayisi", formatted=formatted)
 
         def get_daily_dose_count(self, formatted = False):
-            return functions.remove_dots_and_commas(functions.update_vaccination_data(data_arg="gunluksidozusayisi"), formatted)
+            return functions.update_vaccination_data(data_arg="gunluksidozusayisi", formatted=formatted)
 
         def get_last_update(self):
             return functions.update_vaccination_data(data_arg="asidozuguncellemesaati")
 
     class cases:
         def get_daily_case(self, formatted = False):
-            return functions.remove_dots_and_commas(functions.request(
-                "https://api.thingspeak.com/apps/thinghttp/send_request?api_key=5T7CBZG02TEYNMS1"
-            ), formatted)
+            return functions.request(
+                "https://api.thingspeak.com/apps/thinghttp/send_request?api_key=5T7CBZG02TEYNMS1",
+                formatted=formatted
+            )
 
         def get_average_cases_per_day(self, formatted = False):
             data = functions.request(
                 "https://api.thingspeak.com/apps/thinghttp/send_request?api_key=ABOIJZXZDK7FTWVC",
-                "html",
+                "html", formatted=formatted
             )
-            return functions.remove_dots_and_commas(data[0].text, formatted)
+            return data[0].text
             
 
     class deaths_and_recovered:
         def get_daily_death(self, formatted = False):
-            return functions.remove_dots_and_commas(functions.request(
-                "https://api.thingspeak.com/apps/thinghttp/send_request?api_key=JN6N1R2OSFU5LGO7"
-            ), formatted)
+            return functions.request(
+                "https://api.thingspeak.com/apps/thinghttp/send_request?api_key=JN6N1R2OSFU5LGO7", 
+                formatted=formatted
+            )
 
         def get_total_death(self, formatted = False):
-            return functions.remove_dots_and_commas(functions.request(
-                "https://api.thingspeak.com/apps/thinghttp/send_request?api_key=VJR862TXLHUT52JE"
-            ), formatted)
+            return functions.request(
+                "https://api.thingspeak.com/apps/thinghttp/send_request?api_key=VJR862TXLHUT52JE", 
+                formatted=formatted
+            )
          
         def get_average_deaths_per_day(self):
             today = date.today()
@@ -74,6 +77,7 @@ class covid_turkey:
             return None
 
         def get_daily_recovered(self, formatted = False):
-            return functions.remove_dots_and_commas(functions.request(
-                "https://api.thingspeak.com/apps/thinghttp/send_request?api_key=R2550VKLU915XLZ6"
-            ), formatted)
+            return functions.request(
+                "https://api.thingspeak.com/apps/thinghttp/send_request?api_key=R2550VKLU915XLZ6", 
+                formatted=formatted
+            )
--- src/functions.py
+++ src/functions.py
@@ -15,10 +15,8 @@ VALID_VAX_ARGS = {
     "doz4turkiyeortalamasi",
     "asidozuguncellemesaati",
 }
-def remove_dots_and_commas(string, formatted = False):
-    if formatted:
-        return string.replace(".", "").replace(",", "")
-    return string
+def remove_dots_and_commas(string):
+    return string.replace(".", "").replace(",", "")
 
 def parse_js(js_source):
     return js_source.split("\n")
@@ -29,26 +27,29 @@ def parse_html(html_source):
     return soup.find_all()
 
 
-def request(url, source="number"):
+def request(url, source="number", formatted = False):
     response = requests.get(url)
     source_content = response.content.decode("utf-8")
     if source == "html":
         source_content = parse_html(source_content)
     elif source == "js":
         source_content = parse_js(source_content)
-    
+    if formatted:
+        return remove_dots_and_commas(source_content)
     return source_content
 
 
-def update_vaccination_data(data_arg):
+def update_vaccination_data(data_arg, formatted = False):
     data = None
     latest = request(
         "https://api.thingspeak.com/apps/thinghttp/send_request?api_key=FERNZ2C4JZDC8HV9",
-        "js",
+        "js"
     )
     if data_arg not in VALID_VAX_ARGS:
         raise ValueError("Invalid argument")
     for line in latest:
         if line.startswith(f"var {data_arg}"):
             data = line.split("=")[1].replace(";", "").replace("'", "")[1:]
+    if formatted:
+        return remove_dots_and_commas(data)
     return data
